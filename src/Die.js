b81df08375742d4fbd1cf1d068343c125cd752f0
UtkarshChandel <personal.utkarsh24@gmail.com>
2020-04-05 19:43:12 +0530

    fin.

--- src/Die.js
+++ src/Die.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import "./Die.css";
 export default class Die extends Component {
   render() {
-    return <i className={`Die fas fa-dice-${this.props.face}`}></i>;
+    return (
+      <i
+        className={`Die fas fa-dice-${this.props.face} ${
+          this.props.rolling && "shaking"
+        }`}
+      ></i>
+
+      //that `this.props.rolling && "shaking"` taking advantage of the boolean op.
+    );
   }
 }
