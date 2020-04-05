9127e13bbf39673af8e4a51c9f89625fcaa0bd68
UtkarshChandel <personal.utkarsh24@gmail.com>
2020-04-05 20:03:17 +0530

    Updates

---
+++ precache-manifest.3d53a10df7acfa20557c21e083970cdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5be76da692389df6d7abd32473d74a",
+    "url": "/lets_roll/index.html"
+  },
+  {
+    "revision": "633df66ba26b4f92b582",
+    "url": "/lets_roll/static/css/main.06da4658.chunk.css"
+  },
+  {
+    "revision": "ed662c4f9e102541bb74",
+    "url": "/lets_roll/static/js/2.5afc67d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lets_roll/static/js/2.5afc67d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633df66ba26b4f92b582",
+    "url": "/lets_roll/static/js/main.be649280.chunk.js"
+  },
+  {
+    "revision": "24dbaa7a5981eaa7b7bb",
+    "url": "/lets_roll/static/js/runtime-main.71a0f2ed.js"
+  }
+]);
